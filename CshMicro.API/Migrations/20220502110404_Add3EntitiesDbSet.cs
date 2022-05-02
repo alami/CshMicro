f9550c74886a6f61ce41449dcd862fa1cca98ca5
alami <alami@km.ru>
2022-05-02 16:05:17 +0500

    application.json CORRECT & mgrtions

---
+++ CshMicro.API/Migrations/20220502110404_Add3EntitiesDbSet.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CshMicro.API.Migrations
+{
+    public partial class Add3EntitiesDbSet : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Periods",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Periods", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Resolutions",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    DisplayOrder = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resolutions", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "VideoTariffs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Description = table.Column<string>(type: "TEXT", nullable: false),
+                    Price = table.Column<double>(type: "REAL", nullable: false),
+                    Image = table.Column<string>(type: "TEXT", nullable: false),
+                    ResolutionId = table.Column<int>(type: "INTEGER", nullable: false),
+                    PeriodId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_VideoTariffs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_VideoTariffs_Periods_PeriodId",
+                        column: x => x.PeriodId,
+                        principalTable: "Periods",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_VideoTariffs_Resolutions_ResolutionId",
+                        column: x => x.ResolutionId,
+                        principalTable: "Resolutions",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoTariffs_PeriodId",
+                table: "VideoTariffs",
+                column: "PeriodId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_VideoTariffs_ResolutionId",
+                table: "VideoTariffs",
+                column: "ResolutionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "VideoTariffs");
+
+            migrationBuilder.DropTable(
+                name: "Periods");
+
+            migrationBuilder.DropTable(
+                name: "Resolutions");
+        }
+    }
+}
