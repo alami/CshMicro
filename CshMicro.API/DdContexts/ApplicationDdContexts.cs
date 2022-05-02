7d23069a9319a82ad457e26985ad02a0d4966e67
alami <alami@km.ru>
2022-05-02 15:55:19 +0500

    3 entities & DbSet

--- CshMicro.API/DdContexts/ApplicationDdContexts.cs
+++ CshMicro.API/DdContexts/ApplicationDdContexts.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CshMicro.API.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CshMicro.API.DdContexts
 {
@@ -6,7 +7,10 @@ namespace CshMicro.API.DdContexts
     {
         public ApplicationDdContexts(DbContextOptions<ApplicationDdContexts> options) : base(options)
         {
-
         }
+        public DbSet<Resolution> Resolutions { get; set; }
+        public DbSet<Period> Periods { get; set; }
+        public DbSet<VideoTariff> VideoTariffs { get; set; }
+
     }
 }
---
+++ CshMicro.API/Entities/VideoTariff.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CshMicro.API.Entities
+{
+    public class VideoTariff
+    {
+        public VideoTariff()
+        {
+            TempQt = 1;
+        }
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        [Range(1, int.MaxValue)]
+        public double Price { get; set; }
+        public string Image { get; set; }
+        [Display(Name = "Resolution")]
+        public int ResolutionId { get; set; }
+        [ForeignKey("ResolutionId")]
+        public virtual Resolution Resolution { get; set; }
+        [Display(Name = "Period")]
+        public int PeriodId { get; set; }
+        [ForeignKey("PeriodId")]
+        public virtual Period Period { get; set; }
+        [NotMapped]
+        [Range(1, 10000)]
+        public int TempQt { get; set; }
+    }
+}
