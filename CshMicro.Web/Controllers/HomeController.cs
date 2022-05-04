38c227a5c5fbd6da90451331798a0b1ac152e97d
alami <alami@km.ru>
2022-05-04 13:46:59 +0500

    5.14 Login Error: Invalid redirect_uri

--- CshMicro.Web/Controllers/HomeController.cs
+++ CshMicro.Web/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using CshMicro.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -27,6 +28,16 @@ namespace CshMicro.Web.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        [Authorize]
+        public IActionResult Login()
+        {
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Logout()
+        {
+            return SignOut("Cookies", "iodc");
         }
     }
 }
